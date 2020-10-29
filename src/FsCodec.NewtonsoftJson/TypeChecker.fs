81e0fbd9155a2b195fcac500ec7db701d996996e
Antonios Klimis <antonios.klimis@gmail.com>
2020-10-29 13:17:07 +0200

    Tiding up

--- src/FsCodec.NewtonsoftJson/TypeChecker.fs
+++ src/FsCodec.NewtonsoftJson/TypeChecker.fs
@@ -1,16 +1,14 @@
 namespace FsCodec.NewtonsoftJson
 
 open System
-open System.Linq
 
 [<AutoOpen>]
 module TypeCheckerHelpers =
-    let TypesImplementing<'T> (assembly : System.Reflection.Assembly) : Type seq =
+    let TypesImplementing<'T> (assembly: System.Reflection.Assembly): Type seq =
         assembly.GetTypes()
         |> Seq.filter (fun t -> typeof<'T>.IsAssignableFrom(t))
-        |> Seq.filter (fun t  -> not t.IsAbstract)
+        |> Seq.filter (fun t -> not t.IsAbstract)
 
-type TypeChecker (allowMaps: bool) =
-    member this.EnumViolations(t : Type) : string seq =
-       Seq.empty
-
+type TypeChecker(allowMaps: bool) =
+    member this.EnumViolations(t: Type): string seq =
+        Seq.empty
